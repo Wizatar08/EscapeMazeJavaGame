b8529016ed571a95b8bb380e6c66781c7a628dd3
_Wizatar08 <kiyoshijim88@gmail.com>
2021-02-12 00:32:27 -0700

    Created basic menu for editor

--- src/com/wizatar08/escapemaze/menus/Editor.java
+++ src/com/wizatar08/escapemaze/menus/Editor.java
@@ -2,16 +2,27 @@ package com.wizatar08.escapemaze.menus;
 
 import com.wizatar08.escapemaze.game.TileMap;
 import com.wizatar08.escapemaze.helpers.ExternalMapHandler;
+import com.wizatar08.escapemaze.helpers.ui.UI;
 import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.opengl.Texture;
+import static com.wizatar08.escapemaze.helpers.Drawer.*;
+
+import java.util.ArrayList;
+
 import static com.wizatar08.escapemaze.helpers.Drawer.*;
 
 public class Editor {
     // Initialize variables
     private Texture background;
     private TileMap map;
+    private UI editorUI;
+    private ArrayList<UI.Menu> menus;
 
     public Editor() {
+        editorUI = new UI();
+        menus = new ArrayList<>();
+        createMenus();
+        addButtons();
         try {
             map = ExternalMapHandler.LoadMap("map.wtremm");
         } catch (NullPointerException e) {
@@ -36,6 +47,16 @@ public class Editor {
     private void draw() {
         drawQuadTex(background, 0, 0, WIDTH * 2, HEIGHT);
         map.draw();
+        editorUI.draw();
+    }
+
+
+    private void createMenus() {
+        editorUI.createMenu("Tiles1", WIDTH - (64 * 3), 20, (64 * 3), HEIGHT - 64, 3, 11);
+    }
+
+    private void addButtons() {
+        editorUI.getMenu("Tiles1").addButton("Tile1", LoadPNG("tiles/null"));
     }
 
 }
