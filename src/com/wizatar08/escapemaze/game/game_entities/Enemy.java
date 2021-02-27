c6781fc947247de56ebf905092904833843e74c9
_Wizatar08 <kiyoshijim88@gmail.com>
2021-02-27 10:19:46 -0700

    Added enemy paths through .json files

--- src/com/wizatar08/escapemaze/game/game_entities/Enemy.java
+++ src/com/wizatar08/escapemaze/game/game_entities/Enemy.java
@@ -1,4 +1,32 @@
 package com.wizatar08.escapemaze.game.game_entities;
 
 public class Enemy {
+    private int id;
+    private int[][] pathCoords;
+    private int currentPathPoint;
+
+    public Enemy(int id, int[][] path) {
+        this.id = id;
+        this.pathCoords = path;
+        this.currentPathPoint = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int[][] getPathCoords() {
+        return pathCoords;
+    }
+
+    public int getCurrentPathIndex() {
+        return currentPathPoint;
+    }
+
+    public void nextPath() {
+        currentPathPoint++;
+        if (currentPathPoint >= pathCoords.length) {
+            currentPathPoint = 0;
+        }
+    }
 }
---
+++ src/com/wizatar08/escapemaze/game/game_entities/JSONEnemyClass.java
@@ -0,0 +1,50 @@
+package com.wizatar08.escapemaze.game.game_entities;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.annotations.SerializedName;
+import com.wizatar08.escapemaze.game.JSONLevel;
+import org.lwjgl.Sys;
+
+import java.util.ArrayList;
+
+public class JSONEnemyClass {
+    // Initialize variables
+
+    @SerializedName("enemies")
+    private JsonArray enemies;
+
+    private Gson gson;
+
+    public JSONEnemyClass(JsonArray enemies) {
+        this.enemies = enemies;
+    }
+
+    public ArrayList<Enemy> getEnemies() {
+        gson = new Gson();
+        int[][][] enemyList = new int[enemies.size()][1024][2];
+        int index[] = new int[enemies.size()];
+        for (int i = 0; i < enemies.size(); i++) {
+            for (int j = 0; j < enemies.get(i).getAsJsonObject().get("paths").getAsJsonArray().size(); j++) {
+                JsonElement jsonArray = enemies.get(i).getAsJsonObject().get("paths").getAsJsonArray().get(j);
+                for (int k = 0; k < jsonArray.getAsJsonArray().size(); k++) {
+                    enemyList[i][j][k] = jsonArray.getAsJsonArray().get(k).getAsInt();
+                }
+                index[i]++;
+            }
+        }
+        ArrayList<Enemy> enemiesList = new ArrayList<>();
+        for (int i = 0; i < enemies.size(); i++) {
+            int[][] enemyPathList = new int[enemies.get(i).getAsJsonObject().get("paths").getAsJsonArray().size()][2];
+            for(int j = 0; j < enemies.get(i).getAsJsonObject().get("paths").getAsJsonArray().size(); j++) {
+                enemyPathList[j] = enemyList[i][j];
+            }
+            //System.out.println(enemyPathList.length + ", " + enemies.get(i).getAsJsonObject().get("paths").getAsJsonArray().size() + ", " + enemies.size());
+            enemiesList.add(new Enemy(
+                    enemies.get(i).getAsJsonObject().get("id").getAsInt(),
+                    enemyPathList));
+        }
+        return enemiesList;
+    }
+}
