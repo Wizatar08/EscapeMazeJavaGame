bfe2ddd411e2db95526bf05f1a1f2046805678e5
_Wizatar08 <kiyoshijim88@gmail.com>
2021-02-28 01:18:53 -0700

    Added Enemy Type enumerator class

---
+++ src/com/wizatar08/escapemaze/game/game_entities/enemies/EnemyType.java
@@ -0,0 +1,86 @@
+package com.wizatar08.escapemaze.game.game_entities.enemies;
+
+import com.wizatar08.escapemaze.helpers.IDTypes;
+import com.wizatar08.escapemaze.helpers.VariationID;
+
+import java.util.*;
+
+public enum EnemyType {
+    CUBE_SCANNER(new VariationID(IDTypes.ENEMY, "001", "00"), "cube_scanner", new Builder().speed(1.0f).viewDistance(64));
+
+    // Initialize variables
+    private String id;
+    private String texture;
+    private float speed;
+    private int viewDistance;
+
+    public static HashMap<String, EnemyType> ENEMY_IDS;
+
+    EnemyType(VariationID id, String texture, Builder builder) {
+        createIdMapAndArrays();
+        addToMap(id.getFullId(), this);
+        this.id = id.getFullId();
+        this.texture = texture;
+        this.speed = builder.getSpeed();
+        this.viewDistance = builder.getViewDistance();
+    }
+
+    private void createIdMapAndArrays() {
+        if (ENEMY_IDS == null) ENEMY_IDS = new HashMap<>();
+    }
+
+    private void addToMap(String id, EnemyType type) {
+        ENEMY_IDS.put(id, type);
+    }
+
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+    public String getTexture() {
+        return texture;
+    }
+    public float getSpeed() {
+        return speed;
+    }
+    public int getViewDistance() {
+        return viewDistance;
+    }
+
+
+
+    /**
+     * Tile builder class
+     */
+    private static class Builder {
+        private float speed;
+        private int viewDistance;
+
+        /**
+         * Builder constructor. Defines all variables to its default value.
+         */
+        private Builder() {
+            speed = 1.0f;
+            viewDistance = 64;
+        }
+
+        private Builder speed(float speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        private Builder viewDistance(int viewDistance) {
+            this.viewDistance = viewDistance;
+            return this;
+        }
+
+
+        public float getSpeed() {
+            return speed;
+        }
+        public int getViewDistance() {
+            return viewDistance;
+        }
+    }
+}
